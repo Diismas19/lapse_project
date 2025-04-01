a1bcebfd0850db21dd49d9d2f44901f50fb792e6
Vitor Petri <diismas19@gmail.com>
2025-04-01 09:29:26 -0300

    Create lapse.py

---
+++ lapse.py
@@ -0,0 +1,7 @@
+# I still have to decide if I'm going to use functional programming or OOP. I thinking about OOP because all the game is around the 4 stats, hungry, popularity, military and financial, but I'm thinking
+# about how I will code the events, how I will random then, with OOP or functioning.
+
+def main():
+  pass
+
+main()
