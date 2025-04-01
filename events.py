37ae8881854f20ef6eec8a815d5def4d4353057c
Vitor Petri <diismas19@gmail.com>
2025-04-01 09:32:22 -0300

    Create events.py

---
+++ events.py
@@ -0,0 +1,5 @@
+# Thinking about use OOP to code the stats of the run. Hunger, Charisma, Military, Money, every event will change those stats, so the point is how do I code and call the events.
+
+class Stats:
+  def __init__(self,hunger,charisma,military,money):
+    pass
